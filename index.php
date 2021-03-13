2abd7b980dfc07a96f6f7226bf935031b1d3bf31
NicoleSa <nicolesang.nicochan@gmail.com>
2021-03-13 17:40:36 +0000

    Index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SOWISO</title>
+  <meta name="description" content="SOWISO">
+  <meta name="author" content="Nicole Sang-Ajang">
+
+</head>
+
+<body>
+
+</body>
+</html>
+
